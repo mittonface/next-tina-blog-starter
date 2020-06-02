06c2039805cd165fd9b294379f98e73a7e0f6de6
Brent Mitton <bmitton@gatech.edu>
2020-06-02 12:38:14 -0400

    bootstrap simple strapi client

---
+++ components/tina-strapi/tina-strapi-client.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export const STRAPI_JWT = "tina_strapi_jwt";
+export const STRAPI_URL = "http://ec2-3-80-4-78.compute-1.amazonaws.com:1337";
+
+export class TinaStrapiClient {
+  authenticate() {
+    axios
+      .post(`${STRAPI_URL}/auth/local`, {
+        identifier: "testuser",
+        password: "testpassword",
+      })
+      .then((authData) => {
+        Cookies.set(STRAPI_JWT, authData.data.jwt);
+      });
+  }
+}
