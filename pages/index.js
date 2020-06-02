745d5e1f56a41f696326c8dee58c4937722648f7
Brent Mitton <bmitton@gatech.edu>
2020-06-02 10:53:27 -0400

    pull all posts from strapi

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Layout from "../components/layout";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
+import { fetchGraphql } from "../lib/api";
 
 export default function Index({ allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout>
@@ -32,20 +32,30 @@ export default function Index({ allPosts }) {
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
+  const queryResponse = await fetchGraphql(`
+  query {
+    blogPosts {
+      title
+      date
+      slug
+      excerpt
+      coverImage {
+        url
+      }
+      author {
+        name
+      }
+    }
+  }
+  `);
+
+  const allPosts = queryResponse.data.blogPosts;
 
   return {
     props: { allPosts },
-  }
+  };
 }
